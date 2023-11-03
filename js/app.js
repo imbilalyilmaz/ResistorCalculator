f4a56f1d3b9d29537c86c5c78f611030f4b47fa7
Bilal <bilalyilmaz.gs@gmail.com>
2023-11-04 00:23:05 +0300

    Resistor Calculator

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const selectTags = document.querySelectorAll(".selected-digits");
+const multiplierValue = document.getElementById("multiplier-value");
+const bands = document.querySelectorAll(".band");
+
+const calculateButton = document.getElementById("calculate");
+const resultBox = document.getElementById("result-box");
+
+selectTags.forEach((select,index) => {
+    select.addEventListener("change", () => {
+        bands[index].style.background = select.options[select.selectedIndex].text;
+    });
+})
+
+multiplierValue.addEventListener("change", () => {
+    bands[3].style.background = multiplierValue.options[multiplierValue.selectedIndex].text;
+});
+
+calculateButton.addEventListener("click", (e) => {
+    const firstBandSelect = bands[0].querySelector("select");
+    const secondBandSelect = bands[1].querySelector("select");
+    const thirdBandSelect = bands[2].querySelector("select");
+
+    if (multiplierValue.value.includes("k")) {
+        multiplierValueNumber = parseFloat(multiplierValue.value.replace(/[k]/g, '')*1000);
+    }else if (multiplierValue.value.includes("M")) {
+        multiplierValueNumber = parseFloat(multiplierValue.value.replace(/[M]/g, '')*1000000);
+    }else if(multiplierValue.value.includes("g")) {
+        multiplierValueNumber = parseFloat(multiplierValue.value.replace(/[g]/g, '')*1000000000);
+    }else {
+        multiplierValueNumber = parseFloat(multiplierValue.value);
+    }
+
+    
+
+    const result = firstBandSelect.value + secondBandSelect.value + thirdBandSelect.value;
+    let finalResult = parseFloat(result)*multiplierValueNumber;
+    let finalResult2 = finalResult.toString();
+    finalResult2 = finalResult2.replace(/000000000$/, 'g').replace(/000000$/, 'M').replace(/000$/, 'k');
+
+
+    if(firstBandSelect.value != "null" && secondBandSelect.value != "null" && thirdBandSelect.value != "null" && multiplierValue.value != "null") {
+        resultBox.textContent = firstBandSelect.value 
+        + secondBandSelect.value 
+        + thirdBandSelect.value
+        + "x" + multiplierValue.value
+        + "Ω = "
+        + finalResult
+        + "Ω = "
+        + finalResult2
+        + "Ω"
+        ;
+    }
+
+    
+});
